f0f56b4712a401aa010459c953b676e0576bc05c
Luc Arnould <44379883+OverLore@users.noreply.github.com>
2024-04-01 01:30:14 +0200

    Ajout du header et de la premiere section

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,98 @@
+<script></script>
+
+<nav aria-label="Main navigation">
+	<div class="main-container">
+		<div class="title-container">
+			<a href="/" class="title-link">
+				<div class="image-mask"></div>
+				<h1 class="gradient-text">Burger Evasion</h1>
+			</a>
+		</div>
+		<div>
+			<ul class="link-list">
+				<li><a href="/about">À propos</a></li>
+				<li><a href="/services">Services</a></li>
+				<li><a href="/contact">Contact</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
+<style>
+	nav {
+		position: fixed;
+		height: 4rem;
+		display: flex;
+		justify-content: center;
+		top: 0;
+		left: 0;
+		right: 0;
+		z-index: 1000;
+	}
+
+	.main-container {
+		display: flex;
+		justify-content: space-between;
+		align-items: flex-end;
+		width: 100%;
+		height: 100%;
+		margin-inline: 10rem;
+	}
+
+	.title-container {
+	}
+
+	.title-link {
+		display: flex;
+		align-items: center;
+		text-decoration: none;
+		color: inherit;
+		font-weight: bold;
+		font-size: 1.25rem;
+	}
+
+	.title-link img {
+		width: 2rem;
+	}
+
+	.title-link h1 {
+		margin-left: 0.25rem;
+		padding-top: 2px;
+		font-size: 1.25rem;
+	}
+
+	.gradient-text {
+		background-color: var(--primary);
+		background-image: linear-gradient(45deg, var(--primary), var(--primary-variant));
+		background-size: 100%;
+		-webkit-background-clip: text;
+		-moz-background-clip: text;
+		-webkit-text-fill-color: transparent;
+		-moz-text-fill-color: transparent;
+	}
+
+	.image-mask {
+		display: inline-block;
+		width: 2rem;
+		height: 2rem;
+		background-image: linear-gradient(180deg, var(--primary), var(--primary-variant));
+		-webkit-mask-image: url('icon.svg');
+		mask-image: url('icon.svg');
+		mask-size: cover;
+	}
+
+	.link-list {
+		list-style: none;
+		display: flex;
+		gap: 3rem;
+		margin: 0;
+		padding: 0;
+		font-size: 1.25rem;
+		font-weight: bold;
+	}
+
+	.link-list a {
+		color: white;
+		text-decoration: none;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,58 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+	<div class="main-container">
+		<section class="main-section">
+			<img class="main-section-background" src="section1burger.webp" alt="" />
+			<div class="main-text-grid">
+				<div>
+					<h2>Goûtez les meilleurs burgers de votre vie.</h2>
+					<h3>
+						Faîtes l'expérience de véritables burgers audacieux à la viande savoureuse et juteuse.
+						Venez redécouvrir ce classique de la cuisine comme vous ne l'avez jamais gouté.
+					</h3>
+				</div>
+			</div>
+		</section>
+	</div>
+</main>
+
+<style>
+	.main-container {
+		width: 100%;
+		height: 100%;
+		margin-inline: 10rem;
+	}
+
+	.main-section {
+		width: 100vw;
+		height: 100svh;
+	}
+
+	.main-section-background {
+		position: absolute;
+		z-index: -1;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		left: 0;
+		object-fit: cover;
+	}
+
+	.main-text-grid {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		height: 100%;
+		align-items: center;
+		color: white;
+	}
+
+	.main-text-grid h2 {
+		font-size: 5rem;
+	}
+
+	.main-text-grid h3 {
+		margin-top: 2rem;
+		font-weight: 500;
+		font-size: 1.75rem;
+		width: 75%;
+	}
+</style>
