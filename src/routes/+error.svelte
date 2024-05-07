0d0f1b11b8008299cdecb82313f007ab7a719d98
Luc Arnould <44379883+OverLore@users.noreply.github.com>
2024-05-08 00:06:04 +0200

    Ajout d'une page d'erreur

---
+++ src/routes/+error.svelte
@@ -0,0 +1,62 @@
+<main class="chalk-background">
+	<div class="main-container">
+		<div class="content">
+			<h1 class="gradient-text">404</h1>
+			<h2>Page introuvable</h2>
+			<p>Vous avez probablement saisi une mauvaise URL.</p>
+			<a href="/">Retour à l'accueil</a>
+		</div>
+	</div>
+</main>
+
+<style>
+	main {
+		margin-top: 3.5rem;
+
+		background-color: #1c1c1c;
+		display: flex;
+		align-content: center;
+		justify-content: center;
+
+		height: calc(50svh);
+	}
+
+	.main-container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	h1 {
+		color: white;
+		font-size: 8rem;
+		margin: 0;
+	}
+
+	h2 {
+		color: white;
+		font-size: 2rem;
+	}
+
+	p {
+		color: white;
+		font-size: 1rem;
+		margin-top: 5rem;
+	}
+
+	a {
+		color: rgb(255, 113, 24);
+		font-size: 0.9rem;
+	}
+
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		color: white;
+		width: 100vw;
+		padding: 10px;
+	}
+</style>
